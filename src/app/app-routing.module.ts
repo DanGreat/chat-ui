00b7f49d41d27ada4f7823d7380a05bda2693a3c
Daniel Bassey <bassey.daniel@wizerconsulting.com>
2022-10-24 14:38:04 +0100

    Added pages for login and chats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
