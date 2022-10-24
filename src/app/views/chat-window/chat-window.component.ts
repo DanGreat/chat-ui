23833dbfc2a851b8c953dedf785eaf6de482a614
Daniel Bassey <bassey.daniel@wizerconsulting.com>
2022-10-24 21:43:55 +0100

    Implemented functionality for sending messages to chat

--- src/app/views/chat-window/chat-window.component.ts
+++ src/app/views/chat-window/chat-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat-window',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatWindowComponent implements OnInit {
 
+  @Input() messages: any[] = []
+  @Input() peerId: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  indexFn(index: number) {
+    return index
   }
 
 }
